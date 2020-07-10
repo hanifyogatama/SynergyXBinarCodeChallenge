9322b718eb3497b0166315e197b5f5b18d4d74c5
Hanif yogatama <hanifyotama@gmail.com>
2020-07-10 10:59:17 +0700

    still find out cetak flow

--- src/Mart.kt
+++ src/Mart.kt
@@ -1,24 +1,27 @@
 open class Mart {
-    var martName : String =""
-    var pesan : String = ""
+    // var martName : String =""
+    // var pesan : String = ""
 
     private val username = arrayListOf("MasAgus")
     private val password = arrayListOf("apaajaboleh")
     var inputUsername : String = ""
     var inputPassword : String = ""
-    var memberMart = mutableListOf<String>()
-    var nonMemberMart = mutableListOf<String>()
+    var memberMartArray = mutableListOf<String>()
+    var barangMartArray = mutableListOf<String>()
+    var hargaBarangArray = mutableListOf<Int>()
+    var jumlahBarangArray = mutableListOf<Int>()
     var namaBarang = mutableListOf<String>()
+    //    var namaBarang = mutableListOf<String>()
     var inputMember : String =""
     var inputBarang :String =""
     var hargaBarang : Int = 0
     var jumlahBarang : Int = 0
+    var total : Int = 0
 
     //var status : Int = 0
 
     // login
     fun login(){
-
         println("------------------------------")
         println("Selamat datang di synergi mart")
         println("------------------------------")
@@ -64,7 +67,7 @@ open class Mart {
         print("Pilih :")
         when(readLine()){
             "1" -> member()
-            "2" -> nonMember()
+            "2" -> nonMemberInput()
             else -> {
                 println("input yang dimasukkan tidak tersedia")
             }
@@ -78,38 +81,69 @@ open class Mart {
         println("----------------------------")
         print("Input Nama Member : ")
         inputMember = readLine().toString()
-        memberMart.add(inputMember)
+        memberMartArray.add(inputMember)
         println("----------------------------")
         memberInput()
     }
 
     fun memberInput(){
+        println("------------------------------------------")
+        println("Checkout pelanggan member : ${inputMember}")
+        println("------------------------------------------")
         do {
-            var done = true
-            println("------------------------------------------")
-            println("Checkout pelanggan member : ${inputMember}")
-            println("------------------------------------------")
+            var done = 0
             print("Input Nama Barang : ")
             inputBarang = readLine().toString()
-            memberMart.add(inputMember)
+            barangMartArray.add(inputBarang)
+
             print("Input Harga Satuan Barang : ")
             hargaBarang = Integer.valueOf(readLine())
+            hargaBarangArray.add(hargaBarang)
+
             print("Input Jumlah Barang : ")
             jumlahBarang = Integer.valueOf(readLine())
-        } while (done == true)
-            inputAgain()
+            jumlahBarangArray.add(jumlahBarang)
+            print("input barang lagi (y/n) : ")
+        } while (done == 1)
+        when(readLine()){
+            "y" -> memberInput()
+            "n" -> cetak()
+            else -> {
+                println("input yang dimasukkan tidak tersedia")
+            }
+        }
+    }
+
+    fun nonMemberInput(){
+        println("------------------------------")
+        println("Checkout pelanggan non member ")
+        println("------------------------------")
+        print("Input Nama Barang : ")
+        inputBarang = readLine().toString()
+        memberMartArray.add(inputBarang)
+        print("Input Harga Satuan Barang : ")
+        hargaBarang = Integer.valueOf(readLine())
+        print("Input Jumlah Barang : ")
+        jumlahBarang = Integer.valueOf(readLine())
     }
 
 
     fun cetak (){
+        println()
+        println("-------------------------------------------")
+        println("checkout pelanggan member : ${inputMember} ")
+        println("-------------------------------------------")
+        barangMartArray.forEach {
+            print(it)
+        }
+        println("-------------------------------------------")
+
+
 
     }
 
 
-    // non - member
-    fun nonMember(){
-        //
-    }
+
 
 
 
